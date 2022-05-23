c92a38a192a210e1520c47afb881c3b796f0a6b2
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2022-05-22 21:51:22 -0400

    canvas submission

---
+++ javascript/code-challenges/practice.js
@@ -0,0 +1,17 @@
+'Use Script'
+
+const complements = [
+  { franchise: 'TOS', ship: 'Enterprise', captain: 'Christopher Pike', Second: 'Number One'},
+  { franchise: 'TNG', ship: 'Enterprise D', captain: 'Jean-Luc Picard', Second: 'Riker' },
+  { franchise: 'TAS', ship: 'Enterprise', captain: 'James Kirk', Second: 'Spock' },
+  { franchise: 'DS9', ship: 'Defiant', captain: 'Benjamin Sisko', Second: 'Kira' },
+  { franchise: 'VOY', ship: 'Voyager', captain: 'Kathryn Janeway', Second: 'Chakotay'},
+  { franchise: 'ENT', ship: 'Enterprise', captain: 'Jonathan Archer', Second: 'TPol'}  
+]
+for (let i = 0; i < complements.length; i++){
+  console.log(complements[i]);
+}
+
+companies.forEach(function (complements) {
+  console.log(company);
+});
